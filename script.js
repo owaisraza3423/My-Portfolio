a336d0a9f73d33ceb9f90a546a21daa0e458e779
owaisraza <ovii13472@gmail.com>
2025-03-11 01:32:28 +0500

    Experience:added

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ var typed = new Typed('#element', {
     strings: ['A front-end web developer.', 'A web designer.','Video Editor'],
     typeSpeed: 50,
   });
-  document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.querySelector('.bx-menu-alt-right');
-    const navList = document.querySelector('nav ul');
-
-    menuIcon.addEventListener('click', () => {
-        navList.classList.toggle('active');
-    });
-});
+  const menu = document.querySelector('.menu');
+  const navUl = document.querySelector('nav ul');
+  
+  menu.addEventListener('click', () => {
+      navUl.classList.toggle('active');
+  });
+  
