da3962272e3d2466d3eb1174a457e61708745add
owaisraza <ovii13472@gmail.com>
2025-03-11 17:27:18 +0500

    improving:improving styling

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ var typed = new Typed('#element', {
     strings: ['A front-end web developer.', 'A web designer.','Video Editor'],
     typeSpeed: 50,
   });
-  const menu = document.querySelector('.menu');
-  const navUl = document.querySelector('nav ul');
-  
-  menu.addEventListener('click', () => {
-      navUl.classList.toggle('active');
+  const menuIcon = document.querySelector('.bx-menu-alt-right');
+  const navList = document.querySelector('nav ul');
+
+  menuIcon.addEventListener('click', () => {
+      navList.classList.toggle('active');
   });
-  
+
+
